e1ac800fc18e412433b4be154fcb3fbf069ff0cf
antonbriganti <anton.briganti@gmail.com>
2024-07-14 15:40:09 +1000

    Update draft process to include content directory
    This is to try and streamline stuff going forward, so I only ever have
    to worry about referencing a base directory when doing images

--- _drafts/_template.md
+++ _drafts/_template.md
@@ -3,4 +3,5 @@ layout: post
 title: !!Title!!
 description: !!Description!!
 tags: [!!Tags!!]
+contentDir: /public/images/!!Directory!!/
 ---
--- scripts/create-draft.sh
+++ scripts/create-draft.sh
@@ -6,6 +6,9 @@ read -p "Enter file name: " filename
 file="./_drafts/$draftdate$filename.md"
 cp ./_drafts/_template.md $file
 
+# setup content directory
+mkdir "./public/images/$(date +%Y-%m)/$filename"
+
 # fill in draft info
 read -p "Enter title: " blogtitle
 sed -i "" "s/!!Title!!/$blogtitle/g" "$file"
@@ -13,6 +16,8 @@ read -p "Enter description: " blogdescription
 sed -i "" "s/!!Description!!/$blogdescription/g" "$file"
 read -p "Enter tags (format: tag1, tag2, tag3): " blogtags
 sed -i "" "s/!!Tags!!/$blogtags/g" "$file"
+directory="$(date +%Y-%m)\/$filename"
+sed -i "" "s/!!Directory!!/$directory/g" "$file"
 
 # display blog post details
 echo
