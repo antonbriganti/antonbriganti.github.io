9dd46c1a978b6f60fa88175b80f44f9fa0f13a59
antonbriganti <anton.briganti@gmail.com>
2024-12-21 02:48:44 +1100

    Update publish script
    now we do index choice to save me time

--- scripts/publish-draft.sh
+++ scripts/publish-draft.sh
@@ -2,8 +2,15 @@
 
 # select a draft
 echo "drafts available:"
-ls ./_drafts | grep -v template
-read -p "Enter post to publish: " originalfilename
+
+drafts=($(ls ./_drafts | grep -v template))
+for i in "${!drafts[@]}"; do
+    echo "$i: ${drafts[$i]}"
+done
+read -p "Enter post to publish: " file_index
+
+# get filename from array
+originalfilename=${drafts[$file_index]}
 
 # update draft filename with current date
 result=$(echo "$originalfilename" | cut -c12-)
